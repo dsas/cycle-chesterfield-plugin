7b7650f474ab8990ab844c9265582bf98b45ce78
Dean Sas <dean@deansas.org>
2026-04-11 23:09:13 +0100

    Link a grade block to ride-grades page

--- blocks/ride-grade/render.php
+++ blocks/ride-grade/render.php
@@ -17,6 +17,9 @@ if ( ! isset( $grades[ $grade ] ) ) {
 	$grade = '1';
 }
 
+$ride_grades_page = get_page_by_path( 'ride-grades' );
+$ride_grades_link = $ride_grades_page instanceof WP_Post ? get_permalink( $ride_grades_page ) : '';
+
 $wrapper_attributes = get_block_wrapper_attributes(
 	array(
 		'class' => 'wp-block-cycle-chesterfield-ride-grade',
@@ -24,6 +27,17 @@ $wrapper_attributes = get_block_wrapper_attributes(
 );
 ?>
 <div <?php echo $wrapper_attributes; ?>>
-	<strong><?php echo esc_html( $grades[ $grade ]['title'] ); ?></strong>
-	<p><?php echo esc_html( $grades[ $grade ]['description'] ); ?></p>
+	<p>
+		<strong><?php echo esc_html( $grades[ $grade ]['title'] ); ?></strong>
+		<?php if ( $ride_grades_link ) : ?>
+			<a
+				href="<?php echo esc_url( $ride_grades_link ); ?>"
+				aria-label="<?php esc_attr_e( 'Learn more about ride grades', 'cycle-chesterfield' ); ?>"
+				title="<?php esc_attr_e( 'Learn more about ride grades', 'cycle-chesterfield' ); ?>"
+			><?php echo esc_html( ' ⓘ' ); ?></a>
+		<?php endif; ?>
+	</p>
+	<p>
+		<?php echo esc_html( $grades[ $grade ]['description'] ); ?>
+	</p>
 </div>
